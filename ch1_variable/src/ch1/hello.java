571298e56d044fb30ac7480a811db174ac16a848
limsuwon <swjjman@naver.com>
2024-01-30 17:50:52 +0900

    asd

---
+++ ch1_variable/src/ch1/hello.java
@@ -0,0 +1,14 @@
+package ch1;
+
+// 클래스 이름은 대문자로 시작
+// 클래스명은 한글이 가능하나, 영어로 사용 
+
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+    }
+}
---
+++ ch1_variable/src/ch1/hello2.java
@@ -0,0 +1,8 @@
+package ch1;
+
+public class hello2 {
+public static void main(String[] args) {
+    int var1 = 5;
+    System.out.println("내가 좋아하는 숫자는 "+var1);
+}
+}
